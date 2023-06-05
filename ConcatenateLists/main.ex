7752de345d0b491aa5494cbc90234f362e934b0b
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-06-05 14:56:51 -0300

    Concatenate elements of two lists

---
+++ ConcatenateLists/main.ex
@@ -0,0 +1,19 @@
+defmodule ListUtils do
+    def concatenateLists(list1, list2) do
+        concatenateLists(list1, list2, [])
+    end
+
+    defp concatenateLists([head | tail], list2, result) do
+        concatenateLists(tail, list2, [head | result])
+    end
+    defp concatenateLists([], [head | tail], result) do
+        concatenateLists([], tail, [head | result])
+    end
+    defp concatenateLists(_, _, result), do: result
+end
+
+
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+concatenated_lists = ListUtils.concatenateLists(list1, list2)
+IO.inspect(concatenated_lists)
