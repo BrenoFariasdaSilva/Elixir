a8a96aaa42af2a3beecd75c80dcbb5aee9e9f210
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-06-05 14:18:46 -0300

    Sum of the elements of a list

---
+++ ListElementsSum/main.ex
@@ -0,0 +1,10 @@
+defmodule ListUtils do
+    def listSum([]) do
+        0
+    end
+    def listSum([head | tail]) do
+        head + listSum(tail)
+    end
+end
+
+IO.puts ListUtils.listSum([1, 2, 3, 4, 5])
