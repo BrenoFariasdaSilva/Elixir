f33ec3fa0e5b534e27fc5efc31e78ab51bd05487
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-06-12 10:18:35 -0300

    REFACTOR: Updating libraries

--- webscrapping/mix.exs
+++ webscrapping/mix.exs
@@ -1,9 +1,9 @@
-defmodule Webscrapping.MixProject do
+defmodule Webscraping.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :webscrapping,
+      app: :webscraping,
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
@@ -21,10 +21,8 @@ defmodule Webscrapping.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:floki, "~> 0.34.3"},
-      {:httpoison, "~> 2.1"}
+      {:httpoison, "~> 1.4"},
+      {:floki, "~> 0.20.0"}
     ]
   end
 end
